1b83fe13f26db22f621fcc01a4db573563ab99e0
Matthew Rosenberg <Rosenmatt1@gmail.com>
2020-03-21 23:24:29 -0600

    Added annotations folder and variables.ts file

---
+++ annotations/variables.ts
@@ -0,0 +1,41 @@
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined; 
+
+//built in objects
+let now: Date = new Date();
+
+//Array
+let colors: string[] = ['red', 'green', 'blue'];
+let numbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, true, false];
+
+//Classes
+class Car {
+
+}
+
+let car: Car = new Car();
+
+//Object literal
+let point: {x: number; y: number} = {
+  x: 10,
+  y: 20
+}
+
+//Function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i)
+}
+//expect function tot ake in an argument i which is a number and return void
+
+//When to use annotations
+
+// 1.Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates = JSON.parse(json);
+console.log(coordinates) //{x: 10, y:20};
+//Avoid using 'any' at all costs
